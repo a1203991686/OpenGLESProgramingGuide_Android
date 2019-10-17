52ef5d4839f59b9271c32311196d6227a08ceb8d
a1203991686 <a1203991686@126.com>
2019-10-17 11:21:13 +0800

    完成FirstOpenGLESProject，并初始化AirHockey1

---
+++ AirHockey1/app/src/main/java/com/littlecorgi/airhockey1/AirHockeyActivity.kt
@@ -0,0 +1,65 @@
+package com.littlecorgi.airhockey1
+
+import android.app.ActivityManager
+import android.content.Context
+import android.opengl.GLSurfaceView
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class AirHockeyActivity : AppCompatActivity() {
+
+    private lateinit var glSurfaceView: GLSurfaceView
+    private var rendererSet = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        glSurfaceView = GLSurfaceView(this)
+
+        /**
+         * 验证是否支持OpenGL ES 2.0
+         */
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val configurationInfo = activityManager.deviceConfigurationInfo
+        val supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000
+                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                && (Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK build for x86")))
+
+        /**
+         * 配置渲染表面
+         */
+        if (supportsEs2) {
+            // 配置这个surface视图
+            glSurfaceView.setEGLContextClientVersion(2)
+
+            // 配置Renderer
+            glSurfaceView.setRenderer(AirHockeyRenderer())
+            rendererSet = true
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        setContentView(glSurfaceView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (rendererSet)
+            glSurfaceView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (rendererSet)
+            glSurfaceView.onResume()
+    }
+}
---
+++ AirHockey1/app/src/main/java/com/littlecorgi/airhockey1/AirHockeyRenderer.kt
@@ -0,0 +1,21 @@
+package com.littlecorgi.airhockey1
+
+import android.opengl.GLES20.*
+import android.opengl.GLSurfaceView
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class AirHockeyRenderer : GLSurfaceView.Renderer {
+
+    override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
+        glClearColor(1.0F, 0.0F, 0.0F, 0.0F)
+    }
+
+    override fun onSurfaceChanged(p0: GL10?, p1: Int, p2: Int) {
+        glViewport(0, 0, p1, p2)
+    }
+
+    override fun onDrawFrame(p0: GL10?) {
+        glClear(GL_COLOR_BUFFER_BIT)
+    }
+}
